eb5d7677a459fc73feb272df778ce236747881e0
DevMa7e1 <thedevma7e1@gmail.com>
2026-04-17 19:33:36 +0300

    Better time system, the echo function, and fixes

--- main.py
+++ main.py
@@ -12,44 +12,67 @@ sampleMax = 2**sampleBits / 2 - 1
 pv = pvspeaker.PvSpeaker(sampleRate, sampleBits)
 pv.start()
 
+class Time:
+    def __init__(self, start_seconds: float = 0, start_pcm_frames: int = 0):
+        global sampleRate
+        self._time = int(start_seconds * sampleRate) + start_pcm_frames
+    def __call__(self, *args, **kwds):
+        return self.seconds()
+    def seconds(self):
+        global sampleRate
+        return self._time / sampleRate
+    def pcm_frames(self):
+        return self._time
+    def increment(self, step = 1):
+        self._time += step
+
 def mod_or_one(a, b):
     if a == b*2:
         return sampleRate
     else:
         return a % (b*2)
 
-def sine_wave(freq: int, time: int, amplitude: float):
-    return int(round(math.sin(mod_or_one(time, 1/freq*sampleRate/2) / (1/freq*sampleRate/2) * math.pi), 8) * (sampleMax * amplitude))
+def sine_wave(freq: int, time: Time, amplitude: float):
+    if(amplitude <= 1 and amplitude >= 0):
+        return int(round(math.sin(mod_or_one(time.pcm_frames(), 1/freq*sampleRate/2) / (1/freq*sampleRate/2) * math.pi), 8) * (sampleMax * amplitude))
 
-def limit(value, limit):
+def limit(value: int, limit: int):
     if value > limit * sampleMax:
         return int(limit * sampleMax)
     if value < -limit * sampleMax:
         return int(-limit * sampleMax)
     return value
 
-freq, time = 0, 0
+freq, time = 0, Time()
 
-def amplify(value, factor):
+def amplify(value: int, factor: float):
     return limit(int(value * factor), 1)
 
 wave = []
 
+def echo(time: Time, difference: Time):
+    global wave
+    if(time.pcm_frames() >= difference.pcm_frames()):
+        return wave[time.pcm_frames()-difference.pcm_frames()]
+    else:
+        return 0
 def play_sound():
     global textInput, freq, time, wave
     code = textInput.get("1.0", "end-1c")
     wave.clear()
     freq = 330
     for i in code.split("\n"):
+        tabs = 0
         matches = re.match("(.*) ->", i)
-        if(matches != None):
-            code = code.replace(i, f"wave.append({str(matches[1])})")
+        if(matches != None and len(matches.groups())):
+            tabs = i.count('\t')
+            code = code.replace(i, f"{'\t'*tabs}wave.append({str(matches[1])})")
+    time = Time()
     for i in range(sampleRate * 2):
-        time = i
         exec(code)
+        time.increment()
     pv.write(wave)
     pv.flush()
-    
 
 label = tk.Label(window, text="Sound 1")
 textInput = tk.Text()
